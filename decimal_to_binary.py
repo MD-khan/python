86e4451384e9e70c77675da1e2fc0e13a1c08baf
MD-khan <md.monir.khan707@gmail.com>
2021-10-30 12:27:16 -0400

    Create decimal_to_binary.py
    This python script converts a decimal values to binary. 
    Here are the algorithm I've followed:
    
    Find the highest decimal value that is less than or equal to 214. This value is 128.
    Set the bit in the 128 column to 1.
    Subtract 128 from 214 to get a remainder of 86.
    Find the highest decimal value that is less than or equal to 86. This value is 64.
    Set the bit in the 64 column to 1.
    Subtract 64 from 86 to get a remainder of 22.
    Repeat the preceding pattern for each remainder value until the remainder is 0.

---
+++ decimal_to_binary.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Find the highest decimal value that is less than or equal to the given decimal
+# Set the bit in the 128 column to 1.
+# Subtract 128 from given decimal to get a remainder
+# Find the highest decimal value that is less than or equal to the reminder.
+# Set the bit in the next column to 1
+# Subtract 64 from the reminder.
+# Repeat the preceding pattern for each remainder value until the remainder is 0.
+
+def decimal_to_binary( number ):
+    decimal_values = [128, 64, 32, 16, 8, 4, 2, 1]
+    binary_digits = []
+    #input_d=214
+    for decimal in decimal_values:
+        if decimal <= number:
+            binary_digits.append(1)
+            number = number - decimal
+        else:
+            binary_digits.append(0)
+    return binary_digits
+
+
+  print(decimal_to_binary(1))
+  print(decimal_to_binary(192))
+  print(decimal_to_binary(209))
+  
